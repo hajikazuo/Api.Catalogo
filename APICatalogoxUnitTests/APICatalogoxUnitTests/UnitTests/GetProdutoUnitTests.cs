278e7dbebe1411cad13cadd3d86f8c61f3984b6f
Nilton <hajikazuo@outlook.com>
2024-08-13 08:33:30 -0300

    feat testes unitarios

---
+++ APICatalogoxUnitTests/APICatalogoxUnitTests/UnitTests/GetProdutoUnitTests.cs
@@ -0,0 +1,35 @@
+﻿using APICatalogo.Controllers;
+using FluentAssertions;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICatalogoxUnitTests.UnitTests
+{
+    public class GetProdutoUnitTests : IClassFixture<ProdutosUnitTestController>
+    {
+        private readonly ProdutosController _controller;
+
+        public GetProdutoUnitTests(ProdutosUnitTestController controller)
+        {
+            _controller = new ProdutosController(controller.repository, controller.mapper);
+        }
+
+        [Fact]
+        public async Task GetProdutoById_OKResult()
+        {
+            // Arrange
+            var produtoId = 2;
+
+            // Act
+            var data = await _controller.Get(produtoId);
+
+            // Assert
+            data.Result.Should().BeOfType<OkObjectResult>()
+            .Which.StatusCode.Should().Be(200); 
+        }   
+    }
+}
---
+++ APICatalogoxUnitTests/APICatalogoxUnitTests/UnitTests/ProdutosUnitTestController.cs
@@ -0,0 +1,43 @@
+﻿using APICatalogo.Context;
+using APICatalogo.DTOs.Mappings;
+using APICatalogo.Repositories;
+using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace APICatalogoxUnitTests.UnitTests
+{
+    public class ProdutosUnitTestController
+    {
+        public IUnitOfWork repository;
+        public IMapper mapper;
+
+        public static DbContextOptions<AppDbContext> dbContextOptions { get; }
+
+        public static string connectionString =
+            "Server=(localdb)\\mssqllocaldb;Database=APICatalogo;Trusted_Connection=True;MultipleActiveResultSets=true";
+
+        static ProdutosUnitTestController()
+        {
+            dbContextOptions = new DbContextOptionsBuilder<AppDbContext>()
+                .UseSqlServer(connectionString)
+                .Options;
+        }
+
+        public ProdutosUnitTestController()
+        {
+            var config = new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile(new ProdutoDTOMappingProfile());
+            });
+
+            mapper = config.CreateMapper();
+            var context = new AppDbContext(dbContextOptions);
+            repository = new UnitOfWork(context);
+        }   
+    }
+}
